5690741c419a08f4c6676f513a7b9785b9c58b6c
chenwen051116 <TavenChen@outlook.com>
2025-11-16 04:08:31 -0800

    Expand autonomous routine with timed actions
    Added multiple movement and shooting steps to the autonomous routine using a Timer for precise delays. Introduced configurable parameters for forward and turn distances, and implemented intake and shooter logic for multi-stage operation.

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FinalAuto.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/FinalAuto.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.pedropathing.util.Timer;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.command.CommandOpMode;
@@ -38,19 +39,25 @@ import java.util.List;
 public class FinalAuto extends LinearOpMode {
 
     // private MultipleTelemetry telemetry;
-
+    private Timer timer;
     private AutoDrivetrain drivetrain;
     private Intake intake;
     private Shooter shooter;
     //    private Shooter shooter;
     private MyLimelight limeLight;
     private Turret turret;
-    private boolean xJustPressed = false;
-    private boolean xHolding = false;
-    private boolean yJustPressed = false;
-    private boolean yHolding = false;
     private double x, y, rx;
 
+    public static int fwd1 = 1000;
+    public static int turn1 = 400;
+
+    public static int fwd2 = 1000;
+    public static int turn2 = 400;
+    public static int turn3 = 400;
+    public static int turn4 = 400;
+    public static int fwd3 = 1000;
+    public static int fwd4 = 1000;
+    public static int fwd5 = 1000;
 
     @Override
     public void runOpMode() {
@@ -67,10 +74,93 @@ public class FinalAuto extends LinearOpMode {
         turret.initEncoder();
         shooter.setShooterStatus(Shooter.ShooterStatus.Stop);
         telemetry.setMsTransmissionInterval(200);
+        timer = new Timer();
         waitForStart();
         while (opModeIsActive()) {
-            drivetrain.forward(1000);
-            sleep(5000);
+
+            drivetrain.forward(fwd1);
+            timer.resetTimer();
+            while(timer.getElapsedTimeSeconds()<3){
+            }
+            drivetrain.turn(turn1);
+            timer.resetTimer();
+            while(timer.getElapsedTimeSeconds()<3){
+            }
+
+            shooter.setShooterStatus(Shooter.ShooterStatus.Shooting);
+            timer.resetTimer();
+            while(timer.getElapsedTimeSeconds()<5){
+                shooter.periodic();
+                //turret.periodic();
+                limeLight.periodic();
+                intake.periodic();
+                intake.updateAutoShoot(true);
+                intake.updateAutoTrans(shooter.isAtTargetRPM());
+                //intake.updateAutoTrans(true);
+                shooter.updateDis(limeLight.getDis());
+                shooter.updateFocused(true);
+            }
+            intake.updateAutoShoot(false);
+            shooter.setShooterStatus(Shooter.ShooterStatus.Stop);
+
+            drivetrain.turn(-turn1);
+            timer.resetTimer();
+            while(timer.getElapsedTimeSeconds()<3){
+            }
+
+            drivetrain.forward(fwd2);
+            timer.resetTimer();
+            while(timer.getElapsedTimeSeconds()<3){
+            }
+            drivetrain.turn(turn2);
+            timer.resetTimer();
+            while(timer.getElapsedTimeSeconds()<3){
+            }
+            intake.setIntakeState(Intake.IntakeTransferState.Suck_In);
+            drivetrain.forward(fwd3);
+            timer.resetTimer();
+            while(timer.getElapsedTimeSeconds()<5){
+                intake.periodic();
+            }
+            intake.setIntakeState(Intake.IntakeTransferState.Intake_Steady);
+
+            drivetrain.forward(-fwd4);
+            timer.resetTimer();
+            while(timer.getElapsedTimeSeconds()<3){
+            }
+            drivetrain.turn(turn3);
+            timer.resetTimer();
+            while(timer.getElapsedTimeSeconds()<3){
+            }
+
+
+            shooter.setShooterStatus(Shooter.ShooterStatus.Shooting);
+            timer.resetTimer();
+            while(timer.getElapsedTimeSeconds()<5){
+                shooter.periodic();
+                //turret.periodic();
+                limeLight.periodic();
+                intake.periodic();
+                intake.updateAutoShoot(true);
+                intake.updateAutoTrans(shooter.isAtTargetRPM());
+                //intake.updateAutoTrans(true);
+                shooter.updateDis(limeLight.getDis());
+                shooter.updateFocused(true);
+            }
+            intake.updateAutoShoot(false);
+            shooter.setShooterStatus(Shooter.ShooterStatus.Stop);
+
+            drivetrain.turn(turn4);
+            timer.resetTimer();
+            while(timer.getElapsedTimeSeconds()<3){
+            }
+
+            drivetrain.forward(fwd5);
+            timer.resetTimer();
+            while(timer.getElapsedTimeSeconds()<3){
+            }
+            return;
+
 
         }
     }
