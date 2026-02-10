4ad7ffd2a7827653c39b07f7e08e2694639052b0
M1Miketro <michaelchen142857@gmail.com>
2026-02-10 09:44:07 -0500

    Added the `intake` package, which contains a rewritten `Intake` class, a rewritten `DistSensor` class (called `BallSensor`), and a new `Gate` class for the servo gate.

---
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/Intake.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.intake;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake extends SubsystemBase {
+    public enum IntakeState{
+        intakeMotor(0.8),
+        STOP(0),
+        REVERSE(-0.8);
+
+        private final double power;
+
+        IntakeState(double power) {
+            this.power = power;
+        }
+    }
+
+    private final DcMotor intakeMotor;
+    private IntakeState intakeState;
+
+    public Intake(HardwareMap hardwareMap) {
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+        intakeMotor.setDirection(DcMotor.Direction.FORWARD);
+        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        intakeState = IntakeState.STOP;
+    }
+
+    public IntakeState getIntakeState() {
+        return intakeState;
+    }
+
+    public void setIntakeState(IntakeState intakeState) {
+        this.intakeState = intakeState;
+    }
+
+    @Override
+    public void periodic() {
+        intakeMotor.setPower(intakeState.power);
+    }
+}
